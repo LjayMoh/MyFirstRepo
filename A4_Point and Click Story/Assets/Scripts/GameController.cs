08059164f8292d7a4f4c0dbd12fc58cbcf0be276
LjayMoh <2194907@students.wits.ac.za>
2021-10-12 15:22:30 +0200

    My First Commit

---
+++ A4_Point and Click Story/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
